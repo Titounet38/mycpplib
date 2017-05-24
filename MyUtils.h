1219d5d2c7893bc585a969e474451649a150409d
Titounet38 <Titounet38@users.noreply.github.com>
2017-05-24 23:25:21 +0200

    Rename myutils.h to MyUtils.h

--- MyUtils.h
+++ MyUtils.h
@@ -715,7 +715,7 @@ public:
 		aVect<wchar_t> buffer;
 
 		if (tmpWrite) {
-			if (!filePaths) MY_ERROR("Aucun fichier n'a été spécifié");
+			if (!filePaths) MY_ERROR("Aucun fichier n'a Ã©tÃ© spÃ©cifiÃ©");
 			Open();
 		}
 
@@ -1276,7 +1276,7 @@ inline int IsDigit(char str) {
 }
 
 inline int IsAlpha(char str) {
-	if (str == 'µ' || str == '°') return true;
+	if (str == 'Âµ' || str == 'Â°') return true;
 	return isalpha((unsigned char)str);
 }
 
